4f955d2de124cfae9a463bf2540fefd18226d401
Erik Andreazi <andreazi.erik@gmail.com>
2024-11-09 14:05:07 -0300

    fix: Adicionando algumas correcoes sugeridas pelo tfsec

--- alb.tf
+++ alb.tf
@@ -1,6 +1,7 @@
 resource "aws_security_group" "aws_alb_sg" {
-  name   = "${var.projeto}-${var.environment}-alb-sg"
-  vpc_id = data.aws_vpcs.default_vpc.ids[0]
+  name        = "${var.projeto}-${var.environment}-alb-sg"
+  vpc_id      = data.aws_vpcs.default_vpc.ids[0]
+  description = "Allow HTTP and HTTPS traffic"
 
   tags = {
     Name = "${var.projeto}-${var.environment}-alb-sg"
@@ -8,6 +9,7 @@ resource "aws_security_group" "aws_alb_sg" {
 }
 
 resource "aws_security_group_rule" "aws_alb_sg_http" {
+  description       = "Allow HTTP"
   type              = "ingress"
   from_port         = 80
   to_port           = 80
@@ -17,6 +19,7 @@ resource "aws_security_group_rule" "aws_alb_sg_http" {
 }
 
 resource "aws_security_group_rule" "aws_alb_sg_https" {
+  description       = "Allow HTTPS"
   type              = "ingress"
   from_port         = 443
   to_port           = 443
@@ -27,11 +30,12 @@ resource "aws_security_group_rule" "aws_alb_sg_https" {
 
 # Defina o ALB
 resource "aws_lb" "example_alb" {
-  name               = "${var.projeto}-${var.environment}-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.aws_alb_sg.id]
-  subnets            = data.aws_subnets.public_subnets.ids[*]
+  name                       = "${var.projeto}-${var.environment}-alb"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.aws_alb_sg.id]
+  subnets                    = data.aws_subnets.public_subnets.ids[*]
+  drop_invalid_header_fields = true
 
   enable_deletion_protection = false
 
@@ -41,10 +45,11 @@ resource "aws_lb" "example_alb" {
 }
 
 resource "aws_lb_target_group" "deny_tg" {
-  name     = "${var.projeto}-deny"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpcs.default_vpc.ids[0]
+  name        = "${var.projeto}-deny"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpcs.default_vpc.ids[0]
+  target_type = "ip"
 
   health_check {
     enabled = false
@@ -57,11 +62,11 @@ resource "aws_lb_listener" "http_listener" {
   port              = 80
   protocol          = "HTTP"
   default_action {
-    type = "fixed-response"
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Access Denied"
-      status_code  = "403"
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
     }
   }
 }
@@ -71,15 +76,15 @@ resource "aws_lb_listener" "https_listener" {
   load_balancer_arn = aws_lb.example_alb.arn
   port              = 443
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = aws_acm_certificate.cert.arn
 
   default_action {
     type = "fixed-response"
     fixed_response {
-      content_type = "text/plain"
-      message_body = "Access Denied"
-      status_code  = "403"
+      content_type = var.tg_default_action[0]
+      message_body = var.tg_default_action[1]
+      status_code  = var.tg_default_action[2]
     }
   }
 }
